919fa6314ed6e965e8ce375d108edd5fdfe9ab91
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-01-08 22:50:19 +0600

    Login view

---
+++ app/src/main/java/subrota/shuvro/gihubuser_kotlin/view/Login.kt
@@ -0,0 +1,12 @@
+package subrota.shuvro.gihubuser_kotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import subrota.shuvro.gihubuser_kotlin.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
