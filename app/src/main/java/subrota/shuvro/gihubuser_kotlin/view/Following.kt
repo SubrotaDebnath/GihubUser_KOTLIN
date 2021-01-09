f63e290e8fc8b71ba8b07078695a8d65763082c4
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-01-09 23:24:37 +0600

    Following

---
+++ app/src/main/java/subrota/shuvro/gihubuser_kotlin/view/Following.kt
@@ -0,0 +1,12 @@
+package subrota.shuvro.gihubuser_kotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import subrota.shuvro.gihubuser_kotlin.R
+
+class Following : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_following)
+    }
+}
