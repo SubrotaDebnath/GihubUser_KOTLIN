07c4cf516204998988b682a40507e2a3b602b43f
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-01-09 23:27:48 +0600

    Following

---
+++ app/src/main/java/subrota/shuvro/gihubuser_kotlin/viewModel/FollowingViewModel.kt
@@ -0,0 +1,7 @@
+package subrota.shuvro.gihubuser_kotlin.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class FollowingViewModel(application: Application) : AndroidViewModel(application) {
+}
