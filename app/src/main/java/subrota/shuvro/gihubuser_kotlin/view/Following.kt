d4334d366e9be2b31dd35a13835654aca8d44f7f
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-01-09 23:29:36 +0600

    Following

--- app/src/main/java/subrota/shuvro/gihubuser_kotlin/view/Following.kt
+++ app/src/main/java/subrota/shuvro/gihubuser_kotlin/view/Following.kt
@@ -2,11 +2,16 @@ package subrota.shuvro.gihubuser_kotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import subrota.shuvro.gihubuser_kotlin.R
+import subrota.shuvro.gihubuser_kotlin.viewModel.FollowingViewModel
 
 class Following : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_following)
+
+        val viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(this.application)).get(FollowingViewModel::class.java)
     }
 }
